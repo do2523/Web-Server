1bd7707d4c7dac6002a742132a31a5b6f4ebc3a8
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-17 18:39:56 -0400

    download theme and change needed paths

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def my_home():
     return render_template('index.html')
         
     return string
-@app.route("/blog")
-def blog():
-    return "HIIII"
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
