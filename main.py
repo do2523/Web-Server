474144eae357ef88ccc788b3144caba05646a1c9
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-18 10:09:51 -0400

    More pages included

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def my_home():
     return render_template('index.html')
-        
+
+
 @app.route("/about.html")
 def about():
     return render_template('about.html')
 
+
 @app.route("/works.html")
 def works():
     return render_template('works.html')
+
+
+@app.route("/contact.html")
+def works():
+    return render_template('contact.html')
 
 # @app.route("/about.html")
 # def about():
